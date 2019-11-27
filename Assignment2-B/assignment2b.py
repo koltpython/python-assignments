b2fb388f4fb4f7c0f600e5d564bfce0bbc111a26
Hasan Can Aslan <42111960+hasancaslan@users.noreply.github.com>
2019-11-27 16:58:43 +0300

    Update assignment2b.py

--- Assignment2-B/assignment2b.py
+++ Assignment2-B/assignment2b.py
@@ -1,18 +1,22 @@
 import random
+
 # In your code don't directly use numbers, but use this variables
-players = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5']
 payoffs = {}
 players_guesses = {}
 number_of_balls = 100
-red_over_blue_ratio = 0.4
+red_ratio = 0.4
 prize = 20
+number_of_players = 5
 
+# This line chooses random players from our class
+all_possible_players = ['AHMET', 'CEREN', 'GUL SENA', 'HASAN CAN', 'ABDULLAH','AHMET','AHMET','AHMET','ALI','ALI','ALI','ALP','ARDA','ATA','AYSE','BERFIN','BERKER','BEYZA','BINNAZ','CANAN','CEMRE','DEMET','DENIZ','DENIZ','DENIZ','DILARA','DILARA','DORUK','DUYGU','EBRU','ECE','ECEM','EDA','EGEHAN','EKIN','EMIR','EMIRHAN','EZGI','EZGI','FATMANUR','FURKAN','GAMZE','GAMZE','GOKCE','GONCA','HALIS','ILAYDA','IREM','IREM','IRIS','KAMRAN','KEMAL','KUBRA','LACIN','MAHMUT','MAHSA','MARCO','MEHMET','MELIKE','MELIS','MERT','MEVA','MOHSEN','MUSTAFA','NARINSU','NAZ','PELIN','PIETRO','SELIN','SEYIT','SEYMA','SOZERI','SUNDUZ','TUBA','TUTKU','UMUT','YAGMUR','YAREN','YUSUF','ZEYNEP','ZEYNEP']
+players = random.sample(all_possible_players, number_of_players)
 
 # This method creates and returns jar that contains number_of_balls balls and color ratio is color_ratio.
 def create_jar():
     jar = ()
     for i in range(0, number_of_balls):
-        if i < (number_of_balls * red_over_blue_ratio):
+        if i < (number_of_balls * red_ratio):
             jar += ('red',)
         else:
             jar += ('blue',)
@@ -20,7 +24,7 @@ def create_jar():
 
 # This method gives given jar contains mostly blue or red.
 def  jar_contains_mostly():
-    if red_over_blue_ratio <= 0.5:
+    if red_ratio <= 0.5:
         return 'blue'
     else:
         return 'red'
@@ -31,7 +35,9 @@ def print_players_guesses():
         print(f"{player}'s choice is {players_guesses[player]}")
     print()
 
-
+#For each player take the player guess as a string
+#Make sure that the player enters a valid contribution (allowed inputs just red or blue)
+#Inform current player about previous players' choices.
 def take_player_inputs(jar):
     random.shuffle(players)
     print('\n' * 20)
@@ -46,6 +52,7 @@ def take_player_inputs(jar):
         print('\n' * 20)
     players.sort()
 
+#This method calculates payoff of each player, keeps it in a dictionary.
 def calculate_payoffs():
     for player in players:
         if players_guesses[player] == jar_contains_mostly():
@@ -59,12 +66,12 @@ def print_payoffs():
     for player in players:
         print(f'{player} earns ${payoffs[player]}.')
 
+
 # Write to a file named payoffs.csv
 # Make sure that there is a header line [Indicating the column names, eg. Player, Payoff] 
 #   and the lines are ended properly [Hint: put a newline character at the end of each line]
 #   and the you close the file.
 def save_to_file():
-# Your code starts here
   with open('payoffs.csv', 'w') as payoffs_file:
     header = ','.join(['Player', 'Payoff'])
     payoffs_file.write(header + '\n')
@@ -73,7 +80,7 @@ def save_to_file():
         line_str = ', '.join(str_items) + '\n'
         payoffs_file.write(line_str)
     payoffs_file.close()
-  # Your code ends here
+    
 
 jar = create_jar()
 take_player_inputs(jar)
